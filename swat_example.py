6df317b91ee4630a04fe5d853c349a6425d30085
Jairo Rotava <jairo.rotava@gmail.com>
2020-04-21 19:12:16 -0300

    fix swat run output on windows

--- swat_example.py
+++ swat_example.py
@@ -37,7 +37,7 @@ for key, value in filecio.items():
     print(key, value)
 
 # Run swat. Select sync (you wait until finished) or async (runs swat and you can process other things at same time)
-ASYNC_MODE_EXAMPLE = False
+ASYNC_MODE_EXAMPLE = True
 if not ASYNC_MODE_EXAMPLE:
     # Exemplo execução sincrona
     logger.debug("Sync example")
