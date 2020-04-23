4c1733673f1ade6c413be88b274517fa01c6c517
jairo <jairo.rotava@gmail.com>
2020-04-23 11:10:11 -0300

    swatcup for linux

--- swat_example.py
+++ swat_example.py
@@ -30,12 +30,6 @@ swat = SWAT(SWATVersion.SWAT2012REV637)
 
 swat.set_project_folder(os.path.join(current_path, 'swatdata/swat2012/swat2012_rev637/txtinout'))
 
-# Read file.cio
-#filecio = swat.read_file_cio()
-#print('File.cio:')
-#for key, value in filecio.items():
-#    print(key, value)
-
 # Run swat. Select sync (you wait until finished) or async (runs swat and you can process other things at same time)
 ASYNC_MODE_EXAMPLE = True
 if not ASYNC_MODE_EXAMPLE:
@@ -52,6 +46,12 @@ else:
         time.sleep(1)
     logger.debug("Return code:" + str(swat.async_return_code()))
 
+
+# Read file.cio
+filecio = swat.read_file_cio()
+print('File.cio:')
+for key, value in filecio.items():
+    print(key, value)
 
 
 # Read daily pcp1.pcp
